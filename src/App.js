ea4b96463a56e69127da0198c7828e145b071244
Israel Nazareth <israel.fab@hotmail.com>
2022-08-12 14:02:21 -0300

    make header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Header />
       <footer className="footer">Footer</footer>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1 className="title">Olá! Meu nome é Israel.</h1>
+        <p className="paragraph">
+          Tenho 29 anos, moro no Rio de Janeiro, sou Desenvolvedor Web, apaixonado por tecnologia, computadores, iniciativas que causem impacto positivo na vida das pessoas de forma inovadora e amante da música.
+        </p>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
