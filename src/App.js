f5656fbe168dcee09cd618fabccb667710ac2a2a
Israel Nazareth <israel.fab@hotmail.com>
2022-08-14 15:35:23 -0300

    makes techs session responsive

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
       <Navbar />
       <Header />
       <Projects />
-      {/* <Techs />
-      <Contact />
+      <Techs />
+      {/* <Contact />
       <Footer /> */}
     </div>
   );
--- src/components/Technologies/Techs.js
+++ src/components/Technologies/Techs.js
@@ -6,38 +6,35 @@ const Techs = () => {
   return (
     <div class="techs" id="techs">
       <div className="techs-container">
-        <h1 className="techs-h1">Tecnologias</h1>
-        <div className="techs-content">
-          <div class="techs-data">
-            <FaReact />
-            <SiRedux />
-            <h1 className="techs-name">React e Redux</h1>
-          </div>
-          <div class="techs-data">
-            <FaHtml5 />
-            <FaCss3Alt />
-            <h1 className="techs-name">Html e Css</h1>
-          </div>
-          <div class="techs-data">
-            <FaJs />
-            <FaBootstrap />
-            <h1 className="techs-name">Javascript e Bootstrap</h1>
-          </div>
-          <div class="techs-data">
-            <FaGitAlt />
-            <FaGithub />
-            <h1 className="techs-name">Git e GitHub</h1>
-          </div>
-          <div class="techs-data">
-            <FaNpm />
-            <FaNodeJs />
-            <h1 className="techs-name">Npm e Node.js</h1>
-          </div>
-          <div class="techs-data">
-            <SiMysql />
-            <SiMongodb />
-            <h1 className="techs-name">MySQL e MongoDB</h1>
-          </div>
+        <div class="techs-data">
+          <FaReact />
+          <SiRedux />
+          <h1 className="techs-name">React e Redux</h1>
+        </div>
+        <div class="techs-data">
+          <FaHtml5 />
+          <FaCss3Alt />
+          <h1 className="techs-name">Html e Css</h1>
+        </div>
+        <div class="techs-data">
+          <FaJs />
+          <FaBootstrap />
+          <h1 className="techs-name">Javascript e Bootstrap</h1>
+        </div>
+        <div class="techs-data">
+          <FaGitAlt />
+          <FaGithub />
+          <h1 className="techs-name">Git e GitHub</h1>
+        </div>
+        <div class="techs-data">
+          <FaNpm />
+          <FaNodeJs />
+          <h1 className="techs-name">Npm e Node.js</h1>
+        </div>
+        <div class="techs-data">
+          <SiMysql />
+          <SiMongodb />
+          <h1 className="techs-name">MySQL e MongoDB</h1>
         </div>
       </div>
     </div>
