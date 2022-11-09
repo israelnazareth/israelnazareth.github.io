ff51b02c76806a2824e4d8cd6a5a0ae28a92bb6a
Israel Nazareth <israel.fab@hotmail.com>
2022-11-09 17:20:11 -0300

    adds NLW Copa project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="header-content">
         <h1 className="title">Olá! Meu nome é Israel.</h1>
         <p className="paragraph">
-          Tenho 29 anos, moro no Rio de Janeiro, sou Desenvolvedor Web, apaixonado por tecnologia, computadores, iniciativas que causem impacto positivo na vida das pessoas de forma inovadora e amante da música.
+          Tenho 30 anos, moro no Rio de Janeiro, sou Desenvolvedor Web, apaixonado por tecnologia, computadores, iniciativas que causem impacto positivo na vida das pessoas de forma inovadora e amante da música.
         </p>
       </div>
     </header>
