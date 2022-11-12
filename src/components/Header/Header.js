b6c0c051948f6440cd28ba34bee89551fe36ca5c
Israel Nazareth <israel.fab@hotmail.com>
2022-11-12 00:33:37 -0300

    makes dynamic years old

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,18 @@
 import './Header.css';
+import dayjs from 'dayjs';
 
 const Header = () => {
+  const bDay = dayjs('1992-10-27')
+  const now = dayjs()
+
+  const yearsOld = now.diff(bDay, 'year')
+
   return (
     <header className="header">
       <div className="header-content">
         <h1 className="title">Olá! Meu nome é Israel.</h1>
         <p className="paragraph">
-          Tenho 30 anos, moro no Rio de Janeiro, sou Desenvolvedor Web, apaixonado por tecnologia, computadores, iniciativas que causem impacto positivo na vida das pessoas de forma inovadora e amante da música.
+          Tenho {yearsOld} anos, moro no Rio de Janeiro, sou Desenvolvedor Web, apaixonado por tecnologia, computadores, iniciativas que causem impacto positivo na vida das pessoas de forma inovadora e amante da música.
         </p>
       </div>
     </header>
