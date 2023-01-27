835dfe761c0d3eab893cc321e1de86f99c8cfc0f
Israel Nazareth <israel.fab@hotmail.com>
2023-01-27 17:15:31 -0300

    feat: make project thumbs lighter

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import './App.css';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-AOS.init({
-  duration: 1000,
-  mirror: true,
-})
+AOS.init({ duration: 500 })
 
 const App = () => {
   return (
