48cd158f215b666aba7a77833dab1591204fe1a5
Israel Nazareth <israelsnazareth@gmail.com>
2023-02-01 21:24:43 -0300

    change email contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
       <div className="contact-container">
         <h1 className="contact-title">Tem interesse em trabalhar comigo?</h1>
         <p className="contact-paragraph">Eu estou nas redes sociais listadas no rodapé mas você também pode me enviar um email ou entrar em contato comigo através do WhatsApp.</p>
-        <a href='mailto:israel.fab@hotmail.com' className="link-button">
+        <a href='mailto:israelsnazareth@gmail.com' className="link-button">
           <button className="email-button">Contato</button>
         </a>
       </div>
