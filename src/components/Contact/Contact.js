7d5b2b735d70362284f3b68f7aec3d8c1d66585f
Israel Nazareth <israelsnazareth@gmail.com>
2023-03-16 23:34:02 -0300

    feat: initiates contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,14 +1,60 @@
+import { useState } from 'react';
 import './Contact.css'
 
 const Contact = () => {
+  const [message, setMessage] = useState('');
+
+  function validateField(e, message) {
+    e.target.setCustomValidity(message)
+  }
+
   return (
     <div data-aos="fade-in" className="contact" id="contact">
       <div className="contact-container">
         <h1 className="contact-title">Tem interesse em trabalhar comigo?</h1>
-        <p className="contact-paragraph">Eu estou nas redes sociais listadas no rodapé mas você também pode me enviar um email ou entrar em contato comigo através do WhatsApp.</p>
-        <a href='mailto:israelsnazareth@gmail.com' className="link-button">
-          <button className="email-button">Contato</button>
-        </a>
+        <p className="contact-paragraph">
+          Me mande uma mensagem através do formulário abaixo! Eu também estou presente no LinkedIn e no Whatsapp listados no rodapé.
+        </p>
+        <form onSubmit={() => { }} method="post">
+          <label htmlFor="name">
+            <input
+              type="text"
+              id="name"
+              placeholder="Nome"
+              onInvalid={e => validateField(e, 'Insira o seu nome')}
+              onInput={e => validateField(e, '')}
+              title="Insira o seu nome"
+              required
+            />
+          </label>
+          <label htmlFor="email">
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              onInvalid={e => validateField(e, 'Insira um email válido')}
+              onInput={e => validateField(e, '')}
+              title="Insira um email válido"
+              required
+            />
+          </label>
+          <label htmlFor="message">
+            <textarea
+              id="message"
+              cols="30"
+              rows="10"
+              maxLength="500"
+              placeholder="Mensagem"
+              onInvalid={e => validateField(e, 'Escreva a sua mensagem')}
+              onInput={e => validateField(e, '')}
+              onChange={e => setMessage(e.target.value)}
+              title="Escreva a sua mensagem"
+              required
+            />
+            <p className="chars-counter">{message.length}/500</p>
+          </label>
+          <button className="email-button" type="submit">Enviar</button>
+        </form>
       </div>
     </div>
   )
